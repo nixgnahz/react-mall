fdfef8dc71b8f26a8f5d847626a45c41ce79044f
nixgnahz <surezhangxin@163.com>
2018-07-23 14:21:00 +0800

    增加关于我们

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import './index.scss'
+
+const aboutArr = [
+  'https://p-h5.missfresh.cn/h5_file/2BEB6474AD0611A1658D7B0ADD0A42FD/statics/img/about-us-01.png',
+  'https://p-h5.missfresh.cn/h5_file/2BEB6474AD0611A1658D7B0ADD0A42FD/statics/img/about-us-02.png',
+  'https://p-h5.missfresh.cn/h5_file/2BEB6474AD0611A1658D7B0ADD0A42FD/statics/img/about-us-03.png'
+]
+
 class About extends React.Component {
   render () {
     return (
       <section className='about'>
-      about
+      {aboutArr.map((item, index)=>
+        <img src={item} key={index}/>
+      )}
+        <p className='about-copyright'>
+          <span>Copyright@2004-2018</span>
+          <span>京东JD.com版权所有</span>
+        </p>
       </section>
     )
   }
